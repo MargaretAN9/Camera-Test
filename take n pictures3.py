a3e57f7ee1b2f07fc505e456877e0b4c7ef300ca
MargaretAN9 <37722313+MargaretAN9@users.noreply.github.com>
2018-07-11 07:44:01 +0000

    Add files via upload

---
+++ take n pictures3.py
@@ -0,0 +1,169 @@
+
+
+
+from picamera import PiCamera
+from time import sleep
+from fractions import Fraction
+import cv2
+import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+#camera = PiCamera(resolution=(1280,720 ))
+#camera = PiCamera(resolution=(1280, 720),framerate=(Fraction (1,8)),sensor_mode=3)
+camera = PiCamera(resolution=(1280, 720),framerate=(.1),sensor_mode=3)
+
+#camera = PiCamera(framerate=(30))
+
+
+
+#set wait time should be min 5 sec 
+
+waitime = 4
+
+xrange = 5
+
+sleep(waitime)
+
+
+
+#capture seed picture
+
+
+camera.shutter_speed = 0
+camera.iso = 0
+sleep(waitime)
+camera.capture('/home/pi/Desktop/testexposure.jpg')
+print ("test")
+    
+img = cv2.imread('/home/pi/Desktop/testexposure.jpg')
+#cv2.putText(img,"{0},{1}".format(x,y),(5,50), font, 2, (0,255,0), 9, cv2.LINE_AA)
+
+img = cv2.resize(img, (0, 0), None, .25, .25)
+#print ((x),(y),"{0:10s}".format(str(camera.shutter_speed/10000000)),(camera.iso))
+#print ((x),(y),"{0:10s}".format(str(round(camera.shutter_speed/10000000))),(camera.iso)) 
+
+
+times = [.1,1,10,100]
+
+
+for i in range (1,xrange):
+    
+    sleep(waitime)
+    camera.shutter_speed = int(10000*(times[i-1]))
+    print (i,(camera.shutter_speed/100000))
+    camera.iso = 100
+    print ("test")
+   
+    camera.capture('img{0:02d}.jpg'.format(i))
+    img1 = cv2.imread('img{0:02d}.jpg'.format(i))
+    img1 = cv2.resize(img1, (0, 0), None, .25, .25)
+    img = np.concatenate((img,img1), axis=1)
+
+ 
+
+cv2.imshow("img", img)    
+      
+camera.close()
+
+cv2.waitKey(0)  
+
+
+"""    
+cv2.destroyAllWindows()
+
+for x in range (1,xrange):
+            
+    if x == 1:
+        #capture seed picture
+        camera.shutter_speed = 0
+        camera.iso = 0
+        sleep(waitime)
+        camera.capture('/home/pi/Desktop/testexposure3.jpg')
+        print ("test2")
+    
+        img4 = cv2.imread('/home/pi/Desktop/testexposure3.jpg')
+        cv2.putText(img4,"auto",(5,50), font, 2, (0,255,0), 9, cv2.LINE_AA)
+
+        img4 = cv2.resize(img4, (0, 0), None, .25, .25)
+
+    else:
+        print ("test")    
+        camera.shutter_speed = 0
+        camera.iso = 0
+        sleep(waitime)
+        camera.capture('img5.jpg')
+        img5 = cv2.imread('img5.jpg')
+        cv2.putText(img5,"auto",(5,50), font, 2, (0,255,0), 9, cv2.LINE_AA)
+        img5 = cv2.resize(img5, (0, 0), None, .25, .25)
+        img4 = np.concatenate((img4, img5), axis=1)
+
+        print ((x),(y),"{0:10s}".format(str(round(camera.shutter_speed/100000))),(camera.iso))  
+            
+
+
+
+img3 = np.concatenate((img3, img4), axis=0)
+
+
+
+   
+
+#img3 = cv2.resize(img3, (0, 0), None, .25, .25)
+
+cv2.imshow("image2", img3)
+cv2.imwrite("/home/pi/Desktop/img3.jpg", img3)
+
+
+
+
+#Displaying a Matplotlib Scale Image
+#Imports a file and displays labels (x,y, title} and resolution tick marks
+#requires matplotlib
+
+
+
+
+#Enter input file name
+filein = "/home/pi/Desktop/img3.jpg"
+
+
+image = mpimg.imread("/home/pi/Desktop/img3.jpg")
+plt.imshow(image)
+
+
+
+#if you want  title only
+#plt.axis ("off")
+
+plt.xlabel('shutter_speed')
+plt.ylabel('iso')
+
+
+plt.title('Public Lab Raspberry Pi Test')
+
+plt.show()
+
+def readImagesAndTimes():
+  
+  times = np.array([ 1/30.0, 0.25, 2.5, 15.0 ], dtype=np.float32)
+  
+  filenames = ["img_0.033.jpg", "img_0.25.jpg", "img_2.5.jpg", "img_15.jpg"]
+
+  images = []
+  for filename in filenames:
+    im = cv2.imread(filename)
+    images.append(im)
+  
+  return images, times
+
+if __name__ == '__main__':
+  # Read images and exposure times
+  print("Reading images ... ")
+
+  images, times = readImagesAndTimes()
+  
+"""
---
+++ testhdr.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+
+# Loading exposure images into a list
+img_fn = ["image01.jpg", "image02.jpg", "image03.jpg", "image04.jpg"]
+img_list = [cv2.imread(fn) for fn in img_fn]
+exposure_times = np.array([0.0001, 0.001, 0.01, 0.1], dtype=np.float32)
+print ("test")
+
+# Merge exposures to HDR image
+merge_debvec = cv2.createMergeDebevec()
+hdr_debvec = merge_debvec.process(img_list, times=exposure_times.copy())
+merge_robertson = cv2.createMergeRobertson()
+hdr_robertson = merge_robertson.process(img_list, times=exposure_times.copy())
+print ("test1")
+
+# Tonemap HDR image
+tonemap1 = cv2.createTonemapDurand(gamma=2.2)
+res_debvec = tonemap1.process(hdr_debvec.copy())
+tonemap2 = cv2.createTonemapDurand(gamma=1.3)
+res_robertson = tonemap2.process(hdr_robertson.copy())
+print ("test2")
+
+
+# Exposure fusion using Mertens
+merge_mertens = cv2.createMergeMertens()
+res_mertens = merge_mertens.process(img_list)
+
+print ("test3")
+
+
+# Convert datatype to 8-bit and save
+res_debvec_8bit = np.clip(res_debvec*255, 0, 255).astype('uint8')
+res_robertson_8bit = np.clip(res_robertson*255, 0, 255).astype('uint8')
+res_mertens_8bit = np.clip(res_mertens*255, 0, 255).astype('uint8')
+
+cv2.imwrite("/home/pi/Desktop/ldr_debvec.jpg", res_debvec_8bit)
+cv2.imwrite("/home/pi/Desktop/ldr_robertson.jpg", res_robertson_8bit)
+cv2.imwrite("/home/pi/Desktop/fusion_mertens.jpg", res_mertens_8bit)
+
+# Estimate camera response function (CRF)
+cal_debvec = cv2.createCalibrateDebevec()
+crf_debvec = cal_debvec.process(img_list, times=exposure_times)
+hdr_debvec = merge_debvec.process(img_list, times=exposure_times.copy(), response=crf_debvec.copy())
+cal_robertson = cv2.createCalibrateRobertson()
+crf_robertson = cal_robertson.process(img_list, times=exposure_times)
+hdr_robertson = merge_robertson.process(img_list, times=exposure_times.copy(), response=crf_robertson.copy())
+
+
+cv2.imwrite("/home/pi/Desktop/hdr_debvec.jpg", hdr_debvec)
+cv2.imwrite("/home/pi/Desktop/hdr_robertson.jpg", hdr_robertson)
+cv2.imwrite("/home/pi/Desktop/crf_robertson.jpg", crf_robertson)
+cv2.imwrite("/home/pi/Desktop/crf_debvec.jpg", crf_debvec)
